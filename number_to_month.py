1d202c42028973285b30404421afc4d66c495432
Mateo <dzmateozd@gmail.com>
2024-04-19 10:56:47 -0300

    Done.

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,8 @@
 # Replace the "ANSWER HERE" for your answer
 
+months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    if month > len(months) or month <= 0: 
+        return "error"
+    return months[month-1]
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,41 @@
 # Replace the "ANSWER HERE" for your answer
+from math import sqrt
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    coefficient = b**2 - 4 * a * c
+    
+    if coefficient < 0:
+        return "( )"
+    
+    root1 = (-b + sqrt(coefficient)) / 2
+    root2 = (-b - sqrt(coefficient)) / 2
+
+    if (root1 == root2): return f"({root1})"
+
+    return f"({root1}, {root2})"
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    return a * (x**2) + b * x + c
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    response = "f(x) ="
 
+    if a: response += f" {a} * X^2"
+    if b: response += f"{' + ' if a else ' '}{b} * X"
+    if c: response += f"{' + ' if a or b else ' '}{c}"
 
-def derivation(a, b):
-    return "ANSWER HERE"
+    return response
+
+def derivation(a, b, _):
+    response = f"f'(x) = "
+
+    if not (a or b): return response + "0"
+    
+    if (a): response += f"{a * 2} * X"
+    
+    if (b): 
+        response += f"{' + ' if a else ''}{b}"
+    
+    return response
